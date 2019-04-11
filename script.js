fdfc8763867a133d7a38b7c837041c9a9a215de2
phuocantd <phuocantd@gmail.com>
2019-04-11 22:36:19 +0700

    add checkbox

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ Vue.component('todo-item', {
     // nhận một "prop" (có thể hiểu là một thuộc tính tùy biến) 
     // có tên là "todo".
     props: ['todo', 'index', 'done'],
-    template: '<li   class="list-group-item" ><p v-on:click="tick(index)" v-html="todo" style="float:left"></p> <button v-on:click="del(index)" style="float:right" type="button" class="close">Del</button></li>'
+    template: '<li class="list-group-item d-flex bd-highlight mb-3" ><div class="p-2 bd-highlight"> <input v-on:click="done=!done;todo=(done?`<del>${todo}</del`:todo.substring(5, todo.length - 5));" type="checkbox" ></div><div class="p-2 bd-highlight"><p v-on:click="done=!done;todo=(done?`<del>${todo}</del`:todo.substring(5, todo.length - 5));" v-html="todo" ></p> </div><div  class="ml-auto p-2 bd-highlight"><button  v-on:click="del(index)" type="button" class="close">Del</button></div></li>'
+    //template: '<li   class="list-group-item" ><p v-on:click="tick(index)" v-html="todo" style="float:left"></p> <button v-on:click="del(index)" style="float:right" type="button" class="close">Del</button></li>'
     //template: '<li v-on:click="tick(index)" class="list-group-item" v-html="todo"></li>'
     //template: '<button v-on:click="tick(todo)" type="button" class="list-group-item list-group-item-action">{{ todo }}</button>'
 })
@@ -64,7 +65,7 @@ function check(index) {
     }
 }
 
-function tick(index) {
+/*function tick(index) {
     if (obj.todoList[index].done) {
         obj.todoList[index].work = obj.todoList[index].work.substring(5, obj.todoList[index].work.length - 6);
         obj.todoList[index].done = false;
@@ -73,7 +74,7 @@ function tick(index) {
         obj.todoList[index].done = true;
     }
 
-}
+}*/
 
 function del(index) {
     obj.todoList.splice(index, 1);
