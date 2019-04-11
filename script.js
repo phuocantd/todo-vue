009a821c63afff7a26ab6fff604f78193d4c1bad
phuocantd <phuocantd@gmail.com>
2019-04-12 00:44:29 +0700

    thêm nút update

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ var btn = new Vue({
                 obj.todoList.push({
                     work: inp.message,
                     done: false,
-                    index: obj.todoList.length
+                    index: obj.todoList.length,
+                    edit:false
                 });
                 inp.message = '';
             }
@@ -26,7 +27,7 @@ Vue.component('todo-item', {
     // nhận một "prop" (có thể hiểu là một thuộc tính tùy biến) 
     // có tên là "todo".
     props: ['todo', 'index', 'done'],
-    template: '<li class="list-group-item d-flex bd-highlight mb-3" ><div class="p-2 bd-highlight"> <input v-on:click="done=!done;todo=(done?`<del>${todo}</del`:todo.substring(5, todo.length - 5));" type="checkbox" ></div><div class="p-2 bd-highlight"><p v-on:click="done=!done;todo=(done?`<del>${todo}</del`:todo.substring(5, todo.length - 5));" v-html="todo" ></p> </div><div  class="ml-auto p-2 bd-highlight"><button  v-on:click="del(index)" type="button" class="close">Del</button></div></li>'
+    template: '<li class="list-group-item d-flex bd-highlight mb-3" ><div class="p-2 bd-highlight"> <input v-on:click="done=!done;todo=(done?`<del>${todo}</del`:todo.substring(5, todo.length - 5));" type="checkbox" ></div><div class="p-2 bd-highlight"><p v-on:click="done=!done;todo=(done?`<del>${todo}</del`:todo.substring(5, todo.length - 5));" v-html="todo" ></p> </div><div class="ml-auto p-2 bd-highlight"><button class="btn btn-warning" v-on:click="update(index)" type="button">Update</button><button class="btn btn-dark" v-on:click="del(index)" type="button">Delete</button></div></li>'
     //template: '<li   class="list-group-item" ><p v-on:click="tick(index)" v-html="todo" style="float:left"></p> <button v-on:click="del(index)" style="float:right" type="button" class="close">Del</button></li>'
     //template: '<li v-on:click="tick(index)" class="list-group-item" v-html="todo"></li>'
     //template: '<button v-on:click="tick(todo)" type="button" class="list-group-item list-group-item-action">{{ todo }}</button>'
@@ -37,24 +38,35 @@ var obj = {
         {
             work: "sleep",
             done: false,
-            index: 0
+            index: 0,
+            edit:false
         },
         {
             work: "eat",
             done: false,
-            index: 1
+            index: 1,
+            edit:false
         },
         {
             work: "code",
             done: false,
-            index: 2
+            index: 2,
+            edit:false
         }
     ]
 }
 
 var list = new Vue({
     el: '#list',
-    data: obj
+    data: obj,
+    methods:{
+        del: function(index) {
+            obj.todoList.splice(index, 1);
+            for (let i = index; i < obj.todoList.length; i++) {
+                obj.todoList[i].index = i;
+            }
+        }
+    }
 })
 
 function check(index) {
@@ -75,12 +87,3 @@ function check(index) {
     }
 
 }*/
-
-function del(index) {
-    obj.todoList.splice(index, 1);
-    //var bool=obj.todoList[index+1].done;
-    for (let i = index; i < obj.todoList.length; i++) {
-        obj.todoList[i].index = i;
-    }
-    //obj.todoList[index].done=!bool;
-}
